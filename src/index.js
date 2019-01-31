29c2488ce54d597b111be866003fc51827cb5414
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-01-30 20:35:26 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import ReactDOM from 'react-dom'
 import React, { Component } from 'react'
-const wrapper = document.getElementById("root");
-console.log(what)
-class App extends Component {
-
-    render() {
-        let bar = {
-            name: "john",
-            bar: () => {
-                console.log(this.name)
-                return (<div>hey</div>)
-            }
-        }
-        return bar.bar()
-    }
-}
-ReactDOM.render(<App />, wrapper)
+const root = document.getElementById("root");
+ReactDOM.render(<div>Hello World!</div>, root)
