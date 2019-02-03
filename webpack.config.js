c3431b2a17e76bc669a863b3ebb5cf44a53adba3
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-02-02 19:38:53 -0500

    Added support for react router and made the / path point to public

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,60 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path")
 module.exports = {
-    devServer: {
-        overlay: {
-            warnings: true,
-            errors: true
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html"
-        })
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 30000,
+              name: "[name].[ext]"
+            }
+          }
+        ]
+      }
     ]
-};
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ],
+  output: {
+    path: path.join(__dirname, "build")
+  },
+  entry: {
+    javascript: "./src/index.js",
+    html: "./public/index.html"
+  }
+}
