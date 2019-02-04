f5209eb66b76124f654806d958580fd55e530896
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-02-04 16:06:54 -0500

    added sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const path = require("path")
 module.exports = {
+  devtool: "source-map",
   devServer: {
     overlay: {
       warnings: true,
